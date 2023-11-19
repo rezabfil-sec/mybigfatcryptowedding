a7bf7bb3f60602a93a61ab63da3dddee73ba6996
rezabfil <cryptochingu@gmail.com>
2023-11-19 02:23:51 +0100

    completing overall flow of privacy deployments contracts

---
+++ aztec/sample-dapp/src/intex.test.mjs
@@ -0,0 +1,38 @@
+import { createSandbox } from '@aztec/aztec-sandbox';
+import { Contract, ExtendedNote, Fr, Note, computeMessageSecretHash, createAccount } from '@aztec/aztec.js';
+import { TokenContractArtifact } from '@aztec/noir-contracts/artifacts';
+
+describe('token', () => {
+  // docs:start:setup
+  let pxe, stop, owner, recipient, token;
+  beforeAll(async () => {
+    ({ pxe, stop } = await createSandbox());
+    owner = await createAccount(pxe);
+    recipient = await createAccount(pxe);
+
+    token = await Contract.deploy(owner, TokenContractArtifact, [owner.getCompleteAddress()]).send().deployed();
+
+    const initialBalance = 20n;
+    const secret = Fr.random();
+    const secretHash = await computeMessageSecretHash(secret);
+    const receipt = await token.methods.mint_private(initialBalance, secretHash).send().wait();
+
+    const storageSlot = new Fr(5);
+    const note = new Note([new Fr(initialBalance), secretHash]);
+    const extendedNote = new ExtendedNote(note, owner.getAddress(), token.address, storageSlot, receipt.txHash);
+    await pxe.addNote(extendedNote);
+
+    await token.methods.redeem_shield({ address: owner.getAddress() }, initialBalance, secret).send().wait();
+  }, 120_000);
+
+  afterAll(() => stop());
+  // docs:end:setup
+
+  // docs:start:test
+  it('increases recipient funds on transfer', async () => {
+    expect(await token.methods.balance_of_private(recipient.getAddress()).view()).toEqual(0n);
+    await token.methods.transfer(owner.getAddress(), recipient.getAddress(), 20n, 0).send().wait();
+    expect(await token.methods.balance_of_private(recipient.getAddress()).view()).toEqual(20n);
+  });
+  // docs:end:test
+});
